19d2ce5d5810d78dadfa7c1ceff891f551f458c1
Laura Iordan <laura.iordan80@e-uvt.ro>
2022-12-28 11:16:27 +0200

    First Commit

---
+++ src/main/java/com/library/books/Books.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.library.books;
+
+/**
+ *
+ * @author laura
+ */
+public class Books {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
